a1598f502ea0ddd093998b13d34a4bc25bbc0518
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-29 20:36:13 -0800

    syntax errors fixed, needed spacings before _id

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -21,12 +21,12 @@ contract CrowdFunding {
 
 //Create new Campaign function
     function createCampaign(
-        address_owner, 
-        string memory_title, 
-        string memory_description, 
-        uint256_target, 
-        uint256_deadline, 
-        string memory_image) public returns (uint256) {
+        address _owner, 
+        string memory _title, 
+        string memory _description, 
+        uint256 _target, 
+        uint256 _deadline, 
+        string memory _image) public returns (uint256) {
             Campaign storage campaign = campaigns[numberOfCampaigns];
 
             //check to verify campaign structure requirements
@@ -45,13 +45,13 @@ contract CrowdFunding {
             return numberOfCampaigns - 1;
         }
 
-    function donateToCampaign(uint256_id) public payable {
+    function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
 
         campaign.donators.push(msg.sender);
-        campaigns.donations.push(amount);
+        campaign.donations.push(amount);
 
         (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
@@ -60,7 +60,7 @@ contract CrowdFunding {
         }
     }
 
-    function getDonators(uint256_id) view public returns (address[] memory, uint256[] memory) {
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].donators, campaigns[_id].donations);
         }
 
