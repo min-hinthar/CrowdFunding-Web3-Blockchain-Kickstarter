f997b0486bcc3b15cddada5d02d3d3bb95a2d2ec
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-29 23:22:33 -0800

    Home.jsx, Profile.jsx, CampaignDetails.jsx, CreateCampaign.jsx created in Pages folder, Components Sidebar and Navbar created and imported into App.jsx with working Pages Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Home, Profile, CreateCampaign, CampaignDetails } from './pages';
+import { Sidebar, Navbar } from './components';
 
 const App = () => {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
       <div className='sm:flex hidden mr-10 relative'>
-        Sidebar
+        <Sidebar />
       </div>
 
       <p className='font-xl font-bold'>TEST</p>
 
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        Navbar
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Profile />} />
+          <Route path='/' element={<CreateCampaign />} />
+          <Route path='/' element={<CampaignDetails />} />
+        </Routes>
+
       </div>
     </div>
   )
