d1238832466eb7c5f1a2c2d64cb0e77e3783137c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-30 22:12:51 -0800

    Profile page updated, context/index.js updated with getUserCampaigns function to return filtered user campaigns by address

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -46,6 +46,16 @@ export const StateContextProvider = ({ children }) => {
         return parsedCampaigns;
     }
 
+
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => 
+            campaign.owner === address);
+
+        return filteredCampaigns;
+    }
+
     return (
         <StateContext.Provider
             value={{ 
@@ -53,6 +63,7 @@ export const StateContextProvider = ({ children }) => {
                 contract,
                 connect,
                 getCampaigns,
+                getUserCampaigns,
                 createCampaign: publishCampaign,
             }}
         >
