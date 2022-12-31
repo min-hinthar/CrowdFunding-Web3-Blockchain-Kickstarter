b9983e752f5e87f4cffb7a996d53b0ff44ea6e3e
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-30 23:31:30 -0800

    Fund Card updated with donate amount, title, description and Donate button

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -17,6 +17,10 @@ const CampaignDetails = () => {
 
   const remainingDays = daysLeft(state.deadline);
 
+  const handleDonate = async () => {
+
+  }
+
   return (
     <div>
       {isLoading && 'Loading...'}
@@ -54,19 +58,17 @@ const CampaignDetails = () => {
               </div>
             </div>
           </div>
-
-        <div>
-            <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>
-              Story
-            </h4>
-            <div className='mt-[20px]'>
-              <p className='leading-[26px] text-justify font-epilogue font-normal text-[12px] text-[#808191]'>
-                  {state.description}
-              </p>
-            </div>
-        </div>
-
-        <div>
+          <div>
+              <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>
+                Story
+              </h4>
+              <div className='mt-[20px]'>
+                <p className='leading-[26px] text-justify font-epilogue font-normal text-[12px] text-[#808191]'>
+                    {state.description}
+                </p>
+              </div>
+          </div>
+          <div>
             <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>
               Donators
             </h4>
@@ -80,12 +82,43 @@ const CampaignDetails = () => {
                   No donators yet! You can be the first!
               </p>
               )}
-
             </div>
+          </div>
         </div>
-
-
-
+          {/* FUND CARD */}
+        <div className='flex-1'>
+          <h4 className='font-epilogue font-semibold text-[18px] text-white uppercase'>
+            Fund
+          </h4>
+          <div className='mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]'>
+            <p className='font-epilogue font-medium text-[20px] leading-[30px] text-center text-[#808191]'>
+              Fund the Campaign!
+            </p>
+            <div className='mt-[30px]'>
+              <input 
+                type='number'
+                placeholder='ETH 0.1'
+                step='0.01'
+                className='w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]'
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+              <div className='mt-[20px] p-4 bg-[#13131a] rounded-[10px]'>
+                <h4 className='font-epilogue font-semibold text-[14px] leading-[22px] text-white'>
+                  Back the campaign because you believe in the cause!
+                </h4>
+                <p className='my-[20px] font-epilogue font-normal leading-[22px] text-[#808191]'>
+                  “No person was ever honored for what he received. Honor has been the reward for what he gave.” Calvin Coolidge
+                </p>
+              </div>
+                <CustomButton 
+                  btnType='button'
+                  title='Fund Campaign'
+                  styles='w-full bg-[#8c6dfd]'
+                  handleClick={handleDonate}
+                />
+            </div>
+          </div>
         </div>
       </div>
     </div>
