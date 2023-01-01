01499947abbc8885d320c373fb1e572ee82fd619
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-31 16:51:23 -0800

    Loader.jsx created and imported into CreateCampaign, CampaignDetails for loading rendering during transactions

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 import { useStateContext } from '../context';
-import { CountBox,CustomButton } from '../components';
+import { CountBox,CustomButton, Loader } from '../components';
 import { calculateBarPercentage, daysLeft } from '../utils';
 import { thirdweb } from '../assets';
 
 const CampaignDetails = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
   const { donate, getDonations, contract, address } = useStateContext();
 
   const [isLoading, setIsLoading] = useState(false);
@@ -30,13 +31,13 @@ const CampaignDetails = () => {
     setIsLoading(true);
 
     await donate(state.pId, amount);
-
+    navigate('/')
     setIsLoading(false);
   }
 
   return (
     <div>
-      {isLoading && 'Loading...'}
+      {isLoading && <Loader />}
       
       {/* Image, CountBox */}
       <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
